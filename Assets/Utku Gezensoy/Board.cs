f31aef2cb12287cdee14a7e8bcef617294d0a636
Utku Gezensoy <utkugezensoy@gmail.com>
2024-03-23 02:29:01 +0300

    Matching & Popping Completed - ScoreText Added

--- Assets/Utku Gezensoy/Board.cs
+++ Assets/Utku Gezensoy/Board.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 using System.Linq;
 using System.Threading.Tasks;
 using DG.Tweening;
+using Debug = UnityEngine.Debug;
+using Random = UnityEngine.Random;
 
 public class Board : MonoBehaviour
 {
@@ -41,6 +43,12 @@ public class Board : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.A)) return;
+        foreach (var connectedTile in Tiles[0, 0].GetConnectedTiles()) connectedTile.icon.transform.DOScale(1.25f, TweenDuration).Play();
+    }
+
     public async void Select(Tile tile)
     {
         if (!_selection. Contains(tile)) _selection.Add(tile);
@@ -48,6 +56,15 @@ public class Board : MonoBehaviour
         Debug.Log(message: $"Selected tiles at ({_selection[0].x}, {_selection[0].y}) and ({_selection[1].x}, {_selection[1].y})");
 
         await Swap(_selection[0], _selection[1]);
+
+        if(CanPop())
+        {
+            Pop();
+        }
+        else
+        {
+            await Swap(_selection[0], _selection[1]);
+        }
 
         _selection.Clear();
     }
@@ -77,6 +94,56 @@ public class Board : MonoBehaviour
         var tile1Item = tile1.Item;
         tile1.Item = tile2.Item;
         tile2.Item = tile1Item;
+    }
+
+    private bool CanPop()
+    {
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                if (Tiles[x, y].GetConnectedTiles().Skip(1).Count() >= 2) 
+                    return true;
+            }
+        }
+        
+        return false;
+    }
+
+    private async Task Pop()
+    {
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                var tile = Tiles[x, y];
+                var connectedTiles = tile.GetConnectedTiles();
+
+                if (connectedTiles.Skip(1).Count() < 2) continue;
+
+                var deflateSequence = DOTween.Sequence();
+
+                foreach (var connectedTile in connectedTiles) deflateSequence.Join(connectedTile.icon.transform.DOScale(Vector3.zero, TweenDuration));
+
+                await deflateSequence.Play() // Sequence
+                                     .AsyncWaitForCompletion(); // Task
+
+                ScoreCounter.Instance.Score += tile.Item.value * connectedTiles.Count;
+
+                var inflateSequence = DOTween.Sequence();
+
+                foreach (var connectedTile in connectedTiles)
+                {
+                    connectedTile.Item = ItemDatabase.Items[Random.Range(0, ItemDatabase.Items.Length)];
+                    inflateSequence.Join(connectedTile.icon.transform.DOScale(Vector3.one, TweenDuration));
+                }
+                
+                await inflateSequence.Play() // Sequence
+                                     .AsyncWaitForCompletion(); // Task
+            }
+       
+        }
+        
     }
 
 }
