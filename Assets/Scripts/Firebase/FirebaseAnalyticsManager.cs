6945d4b68d08ffbaa7917e0e2f6b617a80d9e763
YusufAfacan <y.afacan@outlook.com.tr>
2024-03-26 17:10:25 +0300

    Firebase Analytics Added
    Firebase Analytics SDK Added
    Firebase Analytics  infrastructure is set

---
+++ Assets/Scripts/Firebase/FirebaseAnalyticsManager.cs
@@ -0,0 +1,48 @@
+using Firebase;
+using Firebase.Analytics;
+using Firebase.Extensions;
+using UnityEngine;
+
+public class FirebaseAnalyticsManager : MonoBehaviour
+{
+    public static FirebaseAnalyticsManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            FirebaseApp app = FirebaseApp.DefaultInstance;
+        });
+
+    }
+
+    public void SendLevelCompletedEvent(int level, int totalMoves, float spendTimeInSeconds)
+    {
+        Parameter levelParameter = new("level_parameter", level);
+        Parameter totalMovesParameter = new("total_moves_parameter", totalMoves);
+        Parameter spendTimeInSecondsParameter = new("spend_time_in_seconds", spendTimeInSeconds);
+
+        Parameter[] parameters = { levelParameter, totalMovesParameter, spendTimeInSecondsParameter };
+
+        FirebaseAnalytics.LogEvent("level_completed", parameters);
+    }
+
+    public void SendLevelQuitedEvent(int level, int totalMoves, float spendTimeInSeconds)
+    {
+        Parameter levelParameter = new("level_parameter", level);
+        Parameter totalMovesParameter = new("total_moves_parameter", totalMoves);
+        Parameter spendTimeInSecondsParameter = new("spend_time_in_seconds", spendTimeInSeconds);
+
+        Parameter[] parameters = { levelParameter, totalMovesParameter, spendTimeInSecondsParameter };
+
+        FirebaseAnalytics.LogEvent("level_completed", parameters);
+    }
+
+}
